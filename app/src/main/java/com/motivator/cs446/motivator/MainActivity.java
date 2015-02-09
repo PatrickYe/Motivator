c0cc91e34274a27c0dd925623c23cf91914f5c83
Jacob <jacob@madapparelinc.com>
2015-02-08 19:55:08 -0500

    basic first page

--- app/src/main/java/com/motivator/cs446/motivator/MainActivity.java
+++ app/src/main/java/com/motivator/cs446/motivator/MainActivity.java
@@ -1,19 +1,60 @@
 package com.motivator.cs446.motivator;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    final ArrayList<Task> list = new ArrayList<Task>();
+    StableArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        final ListView listview = (ListView) findViewById(R.id.listview);
 
+        adapter = new StableArrayAdapter(this,
+                R.layout.task_cell, list);
+
+
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +75,57 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (id == R.id.add_task) {
+
+            list.add(new Task("Homework", "9:00"));
+
+            adapter.notifyDataSetChanged();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<Task> {
+
+//        HashMap<Task, Integer> mIdMap = new HashMap<Task, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<Task> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+//                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            Task task = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.task_cell, parent, false);
+            }
+            // Lookup view for data population
+            TextView taskName = (TextView) convertView.findViewById(R.id.firstLine);
+            TextView deadline = (TextView) convertView.findViewById(R.id.secondLine);
+            // Populate the data into the template view using the data object
+            taskName.setText(task.title);
+            deadline.setText(task.deadline);
+            // Return the completed view to render on screen
+            return convertView;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            Task item = getItem(position);
+//            return mIdMap.get(item);
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
---
+++ app/src/main/java/com/motivator/cs446/motivator/Task.java
@@ -0,0 +1,15 @@
+package com.motivator.cs446.motivator;
+
+/**
+ * Created by jacobsimon on 2/8/15.
+ */
+public class Task {
+    public String title;
+    public String deadline;
+
+
+    public Task(String title, String deadline) {
+        this.title = title;
+        this.deadline = deadline;
+    }
+}
