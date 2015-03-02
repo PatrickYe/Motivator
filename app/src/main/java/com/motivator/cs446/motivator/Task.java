bf314bde264c57a6df998a8b0475b4993be4d4d3
Graham Mutter <gmutter@uwaterloo.ca>
2015-03-01 21:56:27 -0500

    Task made Serializable to allow Tasks to be passed through Notifications (via Intents)

--- app/src/main/java/com/motivator/cs446/motivator/Task.java
+++ app/src/main/java/com/motivator/cs446/motivator/Task.java
@@ -1,11 +1,12 @@
 package com.motivator.cs446.motivator;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by jacobsimon on 2/8/15.
  */
-public class Task {
+public class Task  implements Serializable {
     public String title;
     public Date deadline;
     enum State {IN_PROGRESS, COMPLETED, FAILED, DELETED};
